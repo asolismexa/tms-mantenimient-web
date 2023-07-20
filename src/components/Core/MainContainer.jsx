dd39617b1be84ff37cc3f68a13f5f204b3d43c42
asolismexa <asolis@logsys.com.mx>
2023-07-20 09:44:50 -0600

    Update: Change MainContainer Width

--- src/components/Core/MainContainer.jsx
+++ src/components/Core/MainContainer.jsx
@@ -1,17 +1,12 @@
-import { Container } from '@mui/material'
-
 export default function MainContainer({ children }) {
   return (
-    <Container
-      component="main"
-      maxWidth="xl"
-      sx={{
-        mt: '4.5rem',
-        overflow: 'auto',
-        borderRadius: '1rem',
+    <main
+      style={{
+        width: '100%',
+        backgroundColor: '#000',
       }}
     >
       {children}
-    </Container>
+    </main>
   )
 }
